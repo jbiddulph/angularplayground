608206e81ecc9229e17fdf3276b2656c169d46d0
John Biddulph <johnbiddulph@BPS-John-MacBook-Pro.local>
2021-03-03 23:36:20 +0000

    first commit

---
+++ src/app/cockpit/cockpit.component.ts
@@ -0,0 +1,34 @@
+import { Component, ElementRef, EventEmitter, OnInit, Output, ViewChild } from '@angular/core';
+
+@Component({
+  selector: 'app-cockpit',
+  templateUrl: './cockpit.component.html',
+  styleUrls: ['./cockpit.component.scss']
+})
+export class CockpitComponent implements OnInit {
+  @Output() serverCreated = new EventEmitter<{serverName: string, serverContent: string}>();
+  @Output() blueprintCreated = new EventEmitter<{serverName: string, serverContent: string}>();
+  // newServerName = '';
+  // newServerContent = '';
+  @ViewChild('serverContentInput', {static: true}) serverContentInput: ElementRef
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+  onAddServer(serverNameInput: HTMLInputElement) {
+    console.log('sCI: ',this.serverContentInput);
+    this.serverCreated.emit({
+      serverName: serverNameInput.value, 
+      serverContent: this.serverContentInput.nativeElement.value
+    });
+  }
+
+  onAddBlueprint(serverNameInput: HTMLInputElement) {
+    this.blueprintCreated.emit({
+      serverName: serverNameInput.value, 
+      serverContent: this.serverContentInput.nativeElement.value
+    });
+  }
+}
